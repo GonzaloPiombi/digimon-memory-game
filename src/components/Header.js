cd9df09c2dcf57eead26b35f6f4a161fae7f414f
Gonzalo <gonzalopiombi@gmail.com>
2022-01-27 14:16:05 -0300

    Create Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Digimon Memory Game</h1>
+      <p>
+        Click on the digimons <strong>BUT</strong> don't click on them twice!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
