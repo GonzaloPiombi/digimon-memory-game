23086205582c30898f713d5f314564c059882ad6
Gonzalo <gonzalopiombi@gmail.com>
2022-01-27 15:14:09 -0300

    Add id.

--- src/components/ImagesInfo.js
+++ src/components/ImagesInfo.js
@@ -16,61 +16,73 @@ const imagesInformation = [
     name: 'Agumon',
     url: agumon,
     isClicked: false,
+    id: 1,
   },
   {
     name: 'Greymon',
     url: greymon,
     isClicked: false,
+    id: 2,
   },
   {
     name: 'MetalGreymon',
     url: metalGreymon,
     isClicked: false,
+    id: 3,
   },
   {
     name: 'WarGreymon',
     url: warGreymon,
     isClicked: false,
+    id: 4,
   },
   {
     name: 'Gabumon',
     url: gabumon,
     isClicked: false,
+    id: 5,
   },
   {
     name: 'Garurumon',
     url: garurumon,
     isClicked: false,
+    id: 6,
   },
   {
     name: 'WereGarurumon',
     url: wereGarurumon,
     isClicked: false,
+    id: 7,
   },
   {
     name: 'MetalGarurumon',
     url: metalGarurumon,
     isClicked: false,
+    id: 8,
   },
   {
     name: 'Patamon',
     url: patamon,
     isClicked: false,
+    id: 9,
   },
   {
     name: 'Angemon',
     url: angemon,
     isClicked: false,
+    id: 10,
   },
   {
     name: 'HolyAngemon',
     url: holyAngemon,
     isClicked: false,
+    id: 11,
   },
   {
     name: 'Seraphimon',
     url: seraphimon,
     isClicked: false,
+    id: 12,
   },
 ];
 
