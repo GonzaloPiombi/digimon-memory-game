b2ea10594309551a2ebe79bfd0d5a41343050b74
Gonzalo <gonzalopiombi@gmail.com>
2022-01-31 11:41:56 -0300

    Filter and display images based on the current level.

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -2,7 +2,13 @@ import Card from './Card';
 
 const CardGrid = (props) => {
   return props.imagesInformation.map((item) => {
-    return <Card key={item.id} digimon={item} onClick={props.onClick} />;
+    if (item.level === 1 && props.level === 1) {
+      return <Card key={item.id} digimon={item} onClick={props.onClick} />;
+    } else if (item.level === 2 && props.level === 2) {
+      return <Card key={item.id} digimon={item} onClick={props.onClick} />;
+    } else if (item.level === 3 && props.level === 3) {
+      return <Card key={item.id} digimon={item} onClick={props.onClick} />;
+    }
   });
 };
 
