9241f6a4ab137cfc8b7b9329a75ad3799781268b
Gonzalo <gonzalopiombi@gmail.com>
2022-01-31 11:39:24 -0300

    Update object with new images.

--- src/components/ImagesInfo.js
+++ src/components/ImagesInfo.js
@@ -1,88 +1,388 @@
+import botamon from '../images/botamon.jpg';
+import koromon from '../images/koromon.jpg';
 import agumon from '../images/agumon.jpg';
 import greymon from '../images/greymon.jpg';
 import metalGreymon from '../images/metal-greymon.jpg';
 import warGreymon from '../images/wargreymon.jpg';
+import punimon from '../images/punimon.jpg';
+import tsunomon from '../images/tsunomon.jpg';
 import gabumon from '../images/gabumon.jpg';
 import garurumon from '../images/garurumon.jpg';
 import wereGarurumon from '../images/weregarurumon.jpg';
 import metalGarurumon from '../images/metalgarurumon.jpg';
+import poyomon from '../images/poyomon.jpg';
+import tokomon from '../images/tokomon.jpg';
 import patamon from '../images/patamon.jpg';
 import angemon from '../images/angemon.jpg';
 import holyAngemon from '../images/holyangemon.jpg';
 import seraphimon from '../images/seraphimon.jpg';
+import yuramon from '../images/yuramon.jpg';
+import tanemon from '../images/tanemon.jpg';
+import palmon from '../images/palmon.jpg';
+import togemon from '../images/togemon.jpg';
+import lillymon from '../images/lillymon.jpg';
+import rosemon from '../images/rosemon.jpg';
+import pichimon from '../images/pichimon.jpg';
+import pukamon from '../images/pukamon.jpg';
+import gomamon from '../images/gomamon.jpg';
+import ikkakumon from '../images/ikkakumon.jpg';
+import zudomon from '../images/zudomon.jpg';
+import vikemon from '../images/vikemon.jpg';
+import yukibotamon from '../images/yukibotamon.jpg';
+import nyaromon from '../images/nyaromon.jpg';
+import plotmon from '../images/plotmon.jpg';
+import tailmon from '../images/tailmon.jpg';
+import angewomon from '../images/angewomon.jpg';
+import holydramon from '../images/holydramon.jpg';
+import bubbmon from '../images/bubbmon.jpg';
+import mochimon from '../images/mochimon.jpg';
+import tentomon from '../images/tentomon.jpg';
+import kabuterimon from '../images/kabuterimon.jpg';
+import atlurkabuterimon from '../images/atlurkabuterimon.jpg';
+import heraklekabuterimon from '../images/heraklekabuterimon.jpg';
+import nyokimon from '../images/nyokimon.jpg';
+import pyocomon from '../images/pyocomon.jpg';
+import piyomon from '../images/piyomon.jpg';
+import birdramon from '../images/birdramon.jpg';
+import garudamon from '../images/garudamon.jpg';
+import hououmon from '../images/hououmon.jpg';
 
 const imagesInformation = [
+  {
+    name: 'Botamon',
+    url: botamon,
+    isClicked: false,
+    id: 1,
+    level: 3,
+  },
+  {
+    name: 'Koromon',
+    url: koromon,
+    isClicked: false,
+    id: 2,
+    level: 3,
+  },
   {
     name: 'Agumon',
     url: agumon,
     isClicked: false,
-    id: 1,
+    id: 3,
+    level: 1,
   },
   {
     name: 'Greymon',
     url: greymon,
     isClicked: false,
-    id: 2,
+    id: 4,
+    level: 2,
   },
   {
     name: 'MetalGreymon',
     url: metalGreymon,
     isClicked: false,
-    id: 3,
+    id: 5,
+    level: 2,
   },
   {
     name: 'WarGreymon',
     url: warGreymon,
     isClicked: false,
-    id: 4,
+    id: 6,
+    level: 3,
+  },
+  {
+    name: 'Punimon',
+    url: punimon,
+    isClicked: false,
+    id: 7,
+    level: 3,
+  },
+  {
+    name: 'Tsunomon',
+    url: tsunomon,
+    isClicked: false,
+    id: 8,
+    level: 3,
   },
   {
     name: 'Gabumon',
     url: gabumon,
     isClicked: false,
-    id: 5,
+    id: 9,
+    level: 1,
   },
   {
     name: 'Garurumon',
     url: garurumon,
     isClicked: false,
-    id: 6,
+    id: 10,
+    level: 2,
   },
   {
     name: 'WereGarurumon',
     url: wereGarurumon,
     isClicked: false,
-    id: 7,
+    id: 11,
+    level: 2,
   },
   {
     name: 'MetalGarurumon',
     url: metalGarurumon,
     isClicked: false,
-    id: 8,
+    id: 12,
+    level: 3,
+  },
+  {
+    name: 'Poyomon',
+    url: poyomon,
+    isClicked: false,
+    id: 13,
+    level: 3,
+  },
+  {
+    name: 'Tokomon',
+    url: tokomon,
+    isClicked: false,
+    id: 14,
+    level: 3,
   },
   {
     name: 'Patamon',
     url: patamon,
     isClicked: false,
-    id: 9,
+    id: 15,
+    level: 1,
   },
   {
     name: 'Angemon',
     url: angemon,
     isClicked: false,
-    id: 10,
+    id: 16,
+    level: 2,
   },
   {
     name: 'HolyAngemon',
     url: holyAngemon,
     isClicked: false,
-    id: 11,
+    id: 17,
+    level: 2,
   },
   {
     name: 'Seraphimon',
     url: seraphimon,
     isClicked: false,
-    id: 12,
+    id: 18,
+    level: 3,
+  },
+  {
+    name: 'Yuramon',
+    url: yuramon,
+    isClicked: false,
+    id: 19,
+    level: 3,
+  },
+  {
+    name: 'Tanemon',
+    url: tanemon,
+    isClicked: false,
+    id: 20,
+    level: 3,
+  },
+  {
+    name: 'Palmon',
+    url: palmon,
+    isClicked: false,
+    id: 21,
+    level: 1,
+  },
+  {
+    name: 'Togemon',
+    url: togemon,
+    isClicked: false,
+    id: 22,
+    level: 2,
+  },
+  {
+    name: 'Lillymon',
+    url: lillymon,
+    isClicked: false,
+    id: 23,
+    level: 2,
+  },
+  {
+    name: 'Rosemon',
+    url: rosemon,
+    isClicked: false,
+    id: 24,
+    level: 3,
+  },
+  {
+    name: 'Pichimon',
+    url: pichimon,
+    isClicked: false,
+    id: 25,
+    level: 3,
+  },
+  {
+    name: 'Pukamon',
+    url: pukamon,
+    isClicked: false,
+    id: 26,
+    level: 3,
+  },
+  {
+    name: 'Gomamon',
+    url: gomamon,
+    isClicked: false,
+    id: 27,
+    level: 1,
+  },
+  {
+    name: 'Ikkakumon',
+    url: ikkakumon,
+    isClicked: false,
+    id: 28,
+    level: 2,
+  },
+  {
+    name: 'Zudomon',
+    url: zudomon,
+    isClicked: false,
+    id: 29,
+    level: 2,
+  },
+  {
+    name: 'Vikemon',
+    url: vikemon,
+    isClicked: false,
+    id: 30,
+    level: 3,
+  },
+  {
+    name: 'YukiBotamon',
+    url: yukibotamon,
+    isClicked: false,
+    id: 31,
+    level: 3,
+  },
+  {
+    name: 'Nyaromon',
+    url: nyaromon,
+    isClicked: false,
+    id: 32,
+    level: 3,
+  },
+  {
+    name: 'Plotmon',
+    url: plotmon,
+    isClicked: false,
+    id: 33,
+    level: 1,
+  },
+  {
+    name: 'Tailmon',
+    url: tailmon,
+    isClicked: false,
+    id: 34,
+    level: 2,
+  },
+  {
+    name: 'Angewomon',
+    url: angewomon,
+    isClicked: false,
+    id: 35,
+    level: 2,
+  },
+  {
+    name: 'Holydramon',
+    url: holydramon,
+    isClicked: false,
+    id: 36,
+    level: 3,
+  },
+  {
+    name: 'Bubbmon',
+    url: bubbmon,
+    isClicked: false,
+    id: 37,
+    level: 3,
+  },
+  {
+    name: 'Mochimon',
+    url: mochimon,
+    isClicked: false,
+    id: 38,
+    level: 3,
+  },
+  {
+    name: 'Tentomon',
+    url: tentomon,
+    isClicked: false,
+    id: 39,
+    level: 1,
+  },
+  {
+    name: 'Kabuterimon',
+    url: kabuterimon,
+    isClicked: false,
+    id: 40,
+    level: 2,
+  },
+  {
+    name: 'AtlurKabuterimon',
+    url: atlurkabuterimon,
+    isClicked: false,
+    id: 41,
+    level: 2,
+  },
+  {
+    name: 'HerakleKabuterimon',
+    url: heraklekabuterimon,
+    isClicked: false,
+    id: 42,
+    level: 3,
+  },
+  {
+    name: 'Nyokimon',
+    url: nyokimon,
+    isClicked: false,
+    id: 43,
+    level: 3,
+  },
+  {
+    name: 'Pyocomon',
+    url: pyocomon,
+    isClicked: false,
+    id: 44,
+    level: 3,
+  },
+  {
+    name: 'Piyomon',
+    url: piyomon,
+    isClicked: false,
+    id: 45,
+    level: 1,
+  },
+  {
+    name: 'Birdramon',
+    url: birdramon,
+    isClicked: false,
+    id: 46,
+    level: 2,
+  },
+  {
+    name: 'Garudamon',
+    url: garudamon,
+    isClicked: false,
+    id: 47,
+    level: 2,
+  },
+  {
+    name: 'Hououmon',
+    url: hououmon,
+    isClicked: false,
+    id: 48,
+    level: 3,
   },
 ];
 
